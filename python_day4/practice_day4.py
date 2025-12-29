064b1a74ba9c4b078b1499870950e23e9cae7321
pindari26 <yogit.siel@gmail.com>
2025-12-29 18:22:59 +0530

    To add a practice set a day 4

---
+++ python_day4/practice_day4.py
@@ -0,0 +1,113 @@
+
+# 1.)write a program create a dictionary of hindi with value as their English traslation.provide usser with an option to look it up!
+
+# words={
+#     "maddad":"Help",
+#     "kuta": "dog",
+#     "kitab":"Book",
+#     "khanna":"food"
+# }
+
+# word=input("Enter the word you want meaning of: ")
+
+# print(words[word])
+
+
+
+
+# nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+
+
+
+''' 2.) Write a program ti=o input 8 num. from the user and display all the uique
+numbers(once) '''
+
+
+# s=set()
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+# n=input("Entert number : ")
+# s.add(int(n))
+
+# print(s)
+
+
+
+# ====================================================================
+
+
+# 3.) can we have a set with 18 (int) and" 18 (str) as a vale in it ?
+
+# s=set()
+# s.add("18")
+# s.add(int(18))
+# print(s)
+
+
+# # Output: of this code is {'18', 18}
+
+
+# ===============================================================================
+
+
+# 4.) what will be the lenght of following set s:
+
+# s = set ()
+# s.add(20)
+# s.add(20.0)
+# s.add('20')  # Lenght of s after there operations
+
+# print(s)
+#print(len(s))
+
+
+
+# ================================================================================
+
+
+
+# 5.) we check type of s 
+
+s = {}   # most common question put the interviews
+
+print(type(s))
+
+
+
+# 6.)   Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
+
+# d ={}
+
+# name =input("enter the frnds name: ")
+# language = input("Enter language name: ")
+# d.update({name: language})  # In the case we add same (key)identities entere the update funtions, the value show the last update
+
+# name =input("enter the frnds name: ")
+# language = input("Enter language name: ")
+# d.update({name: language})
+
+# name =input("enter the frnds name: ")
+# language = input("Enter language name: ")
+# d.update({name: language})
+
+# name =input("enter the frnds name: ")
+# language = input("Enter language name: ")
+# d.update({name: language})
+
+# print(d)
+
+
+
+
